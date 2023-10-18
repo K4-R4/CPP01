5e8d53f5b81731faffa8d65d35aabd04776b93a2
K4R4 <mabdman512@gmail.com>
2023-10-19 01:44:03 +0900

    Change ex05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: kura <kura@student.1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 23:49:57 by kura              #+#    #+#             */
-/*   Updated: 2023/10/19 00:39:31 by kura             ###   ########.fr       */
+/*   Updated: 2023/10/19 01:01:52 by kura             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,36 @@
 #include "Harl.hpp"
 
 void Harl::debug() {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
 			  << std::endl;
 }
 
 void Harl::info() {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout
 			<< "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
 			<< std::endl;
 }
 
 void Harl::warning() {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout
 			<< "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
 			<< std::endl;
 }
 
 void Harl::error() {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(std::string level) {
-	std::string avail_levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void (Harl::*avail_funcs[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	const size_t MAX_LEVEL = 4;
+	std::string avail_levels[MAX_LEVEL] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*avail_funcs[MAX_LEVEL])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
-	for (size_t i = 0; i < sizeof(avail_levels) / sizeof(avail_levels[0]); i++) {
+	for (size_t i = 0; i < MAX_LEVEL; i++) {
 		if (level == avail_levels[i]) {
 			(this->*avail_funcs[i])();
 			return;
